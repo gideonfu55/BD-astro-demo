30f8c8200e9447b968e239491a3b6db059add443
jcmonkey87 <fuzhonglong@gmail.com>
2022-07-08 21:59:21 +0800

    added svelte component - increment/decrement count

---
+++ src/Components/Counter.svelte
@@ -0,0 +1,21 @@
+<script>
+    let count = 0;
+
+    function incrementCount() {
+        count += 1;
+    }
+
+    function decrementCount() {
+        count -= 1;
+    }
+
+    function resetCount() {
+        count = 0;
+    }
+</script>
+
+<h1>Counter Element</h1>
+<p>Count: <span>{count}</span></p>
+<button on:click={incrementCount}>+</button>
+<button on:click={decrementCount}>-</button>
+<button on:click={resetCount}>Reset</button>
